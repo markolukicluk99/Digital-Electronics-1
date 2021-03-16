6272c8b64d93f961bf6a1659ade783b9c7c23cb2
Marko Lukić <lukic@netsearch.cz>
2021-03-16 23:42:37 +0100

    Update the project files

---
+++ Labs/05-counter/counter/counter.srcs/sim_1/new/tb_clock_enable.vhd
@@ -0,0 +1,95 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/16/2021 11:29:02 PM
+-- Design Name: 
+-- Module Name: tb_clock_enable - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tb_clock_enable is
+--  Port ( );
+end tb_clock_enable;
+
+architecture Behavioral of tb_clock_enable is
+
+constant c_MAX               : natural := 10;
+    constant c_CLK_100MHZ_PERIOD : time    := 10 ns;
+
+    --Local signals
+    signal s_clk_100MHz : std_logic;
+    signal s_reset      : std_logic;
+    signal s_ce         : std_logic;
+
+begin
+uut_ce : entity work.clock_enable
+        generic map(
+            g_MAX => c_MAX
+        )   -- Note that there is NO comma or semicolon between
+            -- generic map section and port map section
+        port map(
+            clk   => s_clk_100MHz,
+            reset => s_reset,
+            ce_o  => s_ce
+        );
+
+    --------------------------------------------------------------------
+    -- Clock generation process
+    --------------------------------------------------------------------
+    p_clk_gen : process
+    begin
+        while now < 750 ns loop         -- 75 periods of 100MHz clock
+            s_clk_100MHz <= '0';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+            s_clk_100MHz <= '1';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+        end loop;
+        wait;                           -- Process is suspended forever
+    end process p_clk_gen;
+
+    --------------------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------------------
+    p_reset_gen : process
+    begin
+        s_reset <= '0';
+        wait for 28 ns;
+        
+        -- Reset activated
+        s_reset <= '1';
+        wait for 153 ns;
+
+        -- Reset deactivated
+        s_reset <= '0';
+
+        wait;
+    end process p_reset_gen;
+
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+
+end Behavioral;
