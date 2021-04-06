bcf422e6e38fe5e1e7656b8576d7cc8e6db28846
Marko Lukić <lukic@netsearch.cz>
2021-04-06 23:51:21 +0200

    Upload project

---
+++ Labs/08-traffic_lights/traffic_lights/traffic_lights.srcs/sources_1/new/p_smart_traffic_fsm.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/06/2021 11:51:04 PM
+-- Design Name: 
+-- Module Name: p_smart_traffic_fsm - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity p_smart_traffic_fsm is
+--  Port ( );
+end p_smart_traffic_fsm;
+
+architecture Behavioral of p_smart_traffic_fsm is
+
+begin
+
+
+end Behavioral;
